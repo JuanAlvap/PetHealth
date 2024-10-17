176358faa87d0d101731480d1aa37aa02efa86b0
Carlos De la Rosa <candela2920@gmail.com>
2024-10-16 23:36:37 -0500

    primer metodo

--- src/core/PetHealth.java
+++ src/core/PetHealth.java
@@ -28,6 +28,7 @@ public class PetHealth {
     public boolean addOwner(Owner owner) {
         if (!this.owners.contains(owner)) {
             this.owners.add(owner);
+            owner.setPetHealth(this);
             return true;
         }
         return false;
@@ -36,6 +37,7 @@ public class PetHealth {
     public boolean addDoctor(Doctor doctor) {
         if (!this.doctors.contains(doctor)) {
             this.doctors.add(doctor);
+            doctor.setPetHealth(this);
             return true;
         }
         return false;
@@ -66,46 +68,58 @@ public class PetHealth {
         return true;
     }
 
-    public void doctorSummary() {
+    public void valueMostSpent() {
 
     }
 
     public void ownerPetSummary() {
-
+        System.out.println("---------- Owner and Pet Summary ----------");
     }
 
     public Owner ownerMostSpent() {
         return null;
     }
 
-    /*
-    
-    Id: 200200000, Name: Magda Vincent
-- Attendance:
-  - Dogs: 5
-  - Cats: 1
-  - Parrots: 1
-- Treatments:
-  - Vaccinations: 1
-  - Dewormings: 0
-  - Surgeries: 4
-  - General Checkups: 2
-
-    
-     */
-    public void valueMostSpent() {
+    public void doctorSummary() {
         System.out.println("---------- Doctor Summary ----------");
         for(Doctor doctor : this.doctors){
             System.out.println("Id: "+ doctor.getId() + ", Name: "+ doctor.getName());
             System.out.println("- Attendance:");
-            int dogs = 0, cats, parrots, vaccinations, dewormings, surgeries, generals = 0;
+            int dogs = 0, cats = 0, parrots = 0, vaccinations = 0, dewormings = 0, surgeries = 0, generals = 0;
             for(Treatment treatment : doctor.getTreatments()){
                if(treatment.getPet() instanceof Dog){
                    dogs++;
                }
-                
-                
+               else if(treatment.getPet() instanceof Cat){
+                   cats++;
+               }
+               else if(treatment.getPet() instanceof Parrot){
+                   parrots++;
+               }
+               
+               if(treatment.getType() == TreatmentType.VACCINATION){
+                   vaccinations++;
+               }
+               else if(treatment.getType() == TreatmentType.DEWORMING){
+                   dewormings++;
+               }
+               else if(treatment.getType() == TreatmentType.SURGERY){
+                   surgeries++;
+               }
+               else if(treatment.getType() == TreatmentType.GENERAL_CHECKUP){
+                   generals++;
+               }
+                  
             }
+            System.out.println("  - Dogs: " + dogs);
+            System.out.println("  - Cats: " + cats);
+            System.out.println("  - Parrots: "+ parrots);
+            System.out.println("- Treatments: ");
+            System.out.println("  - Vaccinations: " + vaccinations);
+            System.out.println("  - Dewormings: " + dewormings );
+            System.out.println("  - Surgeries: " + surgeries);
+            System.out.println("  -General Checkups:  " + generals);
+            
         }
     }
 
