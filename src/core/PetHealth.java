38b8eb1e32362e7697e48ebb21fad6a13ed30171
Carlos De la Rosa <candela2920@gmail.com>
2024-10-16 23:15:15 -0500

    Avance metodo doctorSummary

--- src/core/PetHealth.java
+++ src/core/PetHealth.java
@@ -46,26 +46,67 @@ public class PetHealth {
     }
 
     public boolean addPet(Pet pet) {
-        if(!this.pets.contains(pet)){
+        if (!this.pets.contains(pet)) {
             this.pets.add(pet);
             return true;
         }
         return false;
     }
-    
-    public Doctor getDoctor(int index){
+
+    public Doctor getDoctor(int index) {
         return this.doctors.get(index);
     }
-    
-    public Pet getPet(int index){
+
+    public Pet getPet(int index) {
         return this.pets.get(index);
     }
-    
-    public boolean createTreatment(Pet pet, ArrayList<Doctor> doctors, TreatmentType type, LocalDate date, double cost){
+
+    public boolean createTreatment(Pet pet, ArrayList<Doctor> doctors, TreatmentType type, LocalDate date, double cost) {
         this.treatments.add(new Treatment(pet, doctors, type, date, cost));
         return true;
     }
+
+    public void doctorSummary() {
+
+    }
+
+    public void ownerPetSummary() {
+
+    }
+
+    public Owner ownerMostSpent() {
+        return null;
+    }
+
+    /*
     
+    Id: 200200000, Name: Magda Vincent
+- Attendance:
+  - Dogs: 5
+  - Cats: 1
+  - Parrots: 1
+- Treatments:
+  - Vaccinations: 1
+  - Dewormings: 0
+  - Surgeries: 4
+  - General Checkups: 2
+
     
+     */
+    public void valueMostSpent() {
+        System.out.println("---------- Doctor Summary ----------");
+        for(Doctor doctor : this.doctors){
+            System.out.println("Id: "+ doctor.getId() + ", Name: "+ doctor.getName());
+            System.out.println("- Attendance:");
+            int dogs = 0, cats, parrots, vaccinations, dewormings, surgeries, generals = 0;
+            for(Treatment treatment : doctor.getTreatments()){
+               if(treatment.getPet() instanceof Dog){
+                   dogs++;
+               }
+                
+                
+            }
+        }
+    }
 
 }
